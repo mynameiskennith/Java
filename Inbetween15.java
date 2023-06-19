74161d0d7a6abc6d8864ca7c728f7e0552ce2bc5
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2023-06-19 12:41:12 +0530

    Add files via upload

---
+++ Inbetween15.java
@@ -0,0 +1,21 @@
+package CS;
+
+import java.util.Scanner;
+
+public class Inbetween15 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int inputNum1 = s.nextInt();
+	
+		
+		if(inputNum1>5) {
+			System.out.println("Hi");
+		}
+
+		s.close();
+	}
+
+}
